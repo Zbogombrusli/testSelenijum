0aaec9403744d29ae05bf81f9cdea145775a708e
Zeljko <38324358+Zbogombrusli@users.noreply.github.com>
2018-06-13 21:34:36 +0200

    testSelenijum

--- src/main.java
+++ src/main.java
@@ -1,20 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- *
- * @author DEATH
- */
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("http://bvtest.school.cubes.rs/login");
+        
+        WebElement emailField= driver.findElement(By.name("email"));
+        
+        emailField.sendKeys("qa@cubes.rs");
+        
+        WebElement passwordField= driver.findElement(By.name("password"));
+        
+        passwordField.sendKeys("cubesqa");
+        
+        WebElement loginButton = driver.findElement(By.className("btn-primary"));
+        loginButton.click();
+        
+        WebElement signaturesLink = driver.findElement(By.linkText("Signatures"));
+        signaturesLink.click();
+
+        //driver.quit();
     }
-    
+
 }
